af0e3e778d11bb6d72ecf7f85cf37ee5e1c4d2d1
LOGEAIS <37186762+LOGEAIS@users.noreply.github.com>
2019-01-04 02:50:13 +0100

    Add files via upload

--- connexion.sql
+++ connexion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 04 jan. 2019 à 01:35
+-- Généré le :  Dim 30 déc. 2018 à 16:38
 -- Version du serveur :  5.7.21
 -- Version de PHP :  5.6.35
 
@@ -34,21 +34,15 @@ CREATE TABLE IF NOT EXISTS `membres` (
   `pseudo` varchar(16) DEFAULT NULL,
   `mdp` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `sexe` int(11) NOT NULL,
-  `profil` int(11) NOT NULL,
-  `type` int(11) NOT NULL,
-  `adresse` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `membres`
 --
 
-INSERT INTO `membres` (`id`, `pseudo`, `mdp`, `email`, `sexe`, `profil`, `type`, `adresse`) VALUES
-(1, 'articque', 'hah7EeXc6dWfA', 'etienne.logeais@efrei.net', 0, 11, 100, '6 rue honorat de bueil'),
-(2, 'etienne', 'hah7EeXc6dWfA', 'etienne.logeais@efrei.net', 1, 1, 0, ''),
-(3, 'coucou', 'hah7EeXc6dWfA', 'et23nba@gmail.com', 1, 1, 0, '');
+INSERT INTO `membres` (`id`, `pseudo`, `mdp`, `email`) VALUES
+(38, 'etienne', 'hah7EeXc6dWfA', 'etienne.logeais@efrei.net');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
